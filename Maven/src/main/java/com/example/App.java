86cc0b1cb01172e0857dbe957a8eae9d044b35fc
Aditya GAikwad <adityagaikwad9972@gmail.com>
2026-05-07 06:48:33 +0530

    Add Maven project

---
+++ Maven/src/main/java/com/example/App.java
@@ -0,0 +1,20 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "This is simple realworld example..." );
+        int a=5;
+        int b=10;
+        System.out.println("sum of "+ a +"and"+b+"is" +sum(a,b));
+    }
+    public static int sum(int x,int y){
+    	return x+y;
+    	}
+}
